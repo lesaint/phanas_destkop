6809ebb0f532328e7d61ac9c287e9b22a580048a
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2020-06-14 10:17:44 +0200

    use PhanNas class, locate home dir, use Path and PurePath instead of str, check mount dir

--- phanas_desktop.py
+++ phanas_desktop.py
@@ -1,63 +1,41 @@
 import gi
-import subprocess
+import os
+import pathlib
 import platform
+import subprocess
 import sys
 import threading
 import time
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
+from pathlib import Path, PurePath
 
 
-script_dir = sys.path[0]
+# from https://stackoverflow.com/a/4028943
+home_dir = Path.home()
+# from https://stackoverflow.com/a/31867043
+script_dir = Path(sys.path[0])
 
-class MyWindow(Gtk.Window):
+class PhanNas:
+    nas_host = "nas"
+
     def __init__(self):
-        Gtk.Window.__init__(self, title="PhanNAS Desktop",
-            default_width=200, resizable=False)
+        self.mount_point_dir = PurePath(str(home_dir) + "/__NAS__")
 
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(self.box)
+    def check_online(self):
+        if self.ping(self.nas_host):
+            print("{} is online".format(self.nas_host))
+        else:
+            print("{} is not online".format(self.nas_host))
 
-        self.label = Gtk.Label("...")
-        self.box.pack_start(self.label, True, True, 0)
-
-        self.connect("show", self.on_window_show)
-
-    def on_window_show(self, widget):
-        print("on_window_show")
-        self.thread = threading.Thread(target=self.do_things)
-        self.thread.daemon = True
-        self.thread.start()
-
-    def do_things(self):
-        self.info_label("Calling test.sh")
-        proc = subprocess.run(script_dir + "/test.sh", 
-            check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("exit code={}\nstdout={}\nstderr={}".format(proc.returncode, proc.stdout, proc.stderr))
-
-        time.sleep(3)
-        self.info_label("Checking NAS is online...")
-        hosts = ["nas", "NAS", "foo", "192.168.1.4", "192.168.1.98"]
-        for host in hosts:
-            if self.ping(host):
-                print("{} is online".format(host))
-            else:
-                print("{} is not online".format(host))
-
-        self.info_label("Connecting NAS drives...")
-        time.sleep(3)
-        self.info_label("All NAS drives connected!")
-        time.sleep(3)
-        GLib.idle_add(Gtk.main_quit)
-
-    def info_label(self, text):
-        GLib.idle_add(self.set_label_text, text)
-
-    def set_label_text(self, text):
-        self.label.set_text(text)
-        # return false to not be called again
-        return False
+    def check_mount_dir(self):
+        if os.path.isdir(self.mount_point_dir):
+            print("mount dir exists")
+            return True
+        else:
+            print("mount dir does not exists")
+            return False
 
     # from https://stackoverflow.com/a/32684938
     def ping(self, host):
@@ -74,6 +52,54 @@ class MyWindow(Gtk.Window):
 
         # call local ping command, suppress stdout and stderr output as we only care about exit code
         return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
+
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="PhanNAS Desktop",
+            default_width=200, resizable=False)
+
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(self.box)
+
+        self.label = Gtk.Label("...")
+        self.box.pack_start(self.label, True, True, 0)
+
+        self.connect("show", self.on_window_show)
+
+        self.phanNAS = PhanNas()
+
+    def on_window_show(self, widget):
+        print("on_window_show")
+        self.thread = threading.Thread(target=self.do_things)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def do_things(self):
+        self.info_label("Calling test.sh")
+        proc = subprocess.run(str(script_dir) + "/test.sh", 
+            check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("exit code={}\nstdout={}\nstderr={}".format(proc.returncode, proc.stdout, proc.stderr))
+
+        time.sleep(3)
+        self.info_label("Checking NAS is online...")
+        self.phanNAS.check_online()
+        self.info_label("Checking mount dir...")
+        if not self.phanNAS.check_mount_dir():
+            return False
+        self.info_label("Connecting NAS drives...")
+        time.sleep(3)
+        self.info_label("All NAS drives connected!")
+        time.sleep(3)
+        GLib.idle_add(Gtk.main_quit)
+
+    def info_label(self, text):
+        GLib.idle_add(self.set_label_text, text)
+
+    def set_label_text(self, text):
+        self.label.set_text(text)
+        # return false to not be called again
+        return False
 
 print(type(script_dir))
 
