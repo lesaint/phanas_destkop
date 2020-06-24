c51e4023e9a9023f191ee1b4fc0a73be3557ccbb
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2020-06-24 23:02:34 +0200

    fix read_credentials_file missing tuples entries

--- phanas/file_utils.py
+++ phanas/file_utils.py
@@ -5,12 +5,12 @@ import stat
 ###Returns tuple (success, msg, username, password)
 def read_credentials_file(credential_file_path):
     if not credential_file_path.is_file():
-        return False, "crediential file is missing"
+        return False, "crediential file is missing", None, None
 
     stats = credential_file_path.stat()
     # see https://stackoverflow.com/a/5337329
     if oct(stats.st_mode)[-3:] != "600":
-        return False, "Permission of {} must be 600".format(credential_file_path)
+        return False, "Permission of {} must be 600".format(credential_file_path), None, None
 
     with open(credential_file_path, 'r') as f:
         user_line_prefix = "username="
@@ -18,18 +18,18 @@ def read_credentials_file(credential_file_path):
 
         user_line = f.readline()
         if not user_line.startswith(user_line_prefix):
-            return False, "Wrong first line in credentials file"
+            return False, "Wrong first line in credentials file", None, None
         # substring without prefix nor ending line return
         username = user_line[len(user_line_prefix):-1]
         if not username:
-            return False, "Missing username in credentials file"
+            return False, "Missing username in credentials file", None, None
 
         pwd_line = f.readline()
         if not pwd_line.startswith(pwd_line_prefix):
-            return False, "Wrong second line in credentials file"
+            return False, "Wrong second line in credentials file", None, None
         pwd = pwd_line[len(pwd_line_prefix):-1]
         if not pwd:
-            return False, "Missing password in credentials file"
+            return False, "Missing password in credentials file", None, None
 
         return True, None, username, pwd
 
